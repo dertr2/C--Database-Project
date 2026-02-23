407bbcea48df76d44e6bb0a927dc7eb0447c1970
Michael Yan <myan2006@students.cs.ubc.ca>
2026-02-23 10:01:08 -0800

    Re Configured application in Program.cs Implemented Swagger UI, added HTTPS redirection for secure links and begun mapping controllers

---
+++ InventoryManagement/Program.cs
@@ -0,0 +1,22 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
+
+
+
