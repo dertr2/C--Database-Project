a0ab331203bfbfb9203d92640c83e70686abc6a7
Michael Yan <myan2006@students.cs.ubc.ca>
2026-02-23 13:47:50 -0800

    added connection to database and listed a set of statements to run to retrieve the table of categories and products joined.

--- InventoryManagement/Controllers/InventoryControllers.cs
+++ InventoryManagement/Controllers/InventoryControllers.cs
@@ -10,24 +10,31 @@ public class ProductController : ControllerBase
     [HttpGet]
     public IActionResult Get()
     {
-        var products = new List<Product>
-        {
-            new Product
-            {
-              Id = 1,  
-              Name = "Apples",
-              Price = 4.50m,
-              Quantity = 46,
-              InStock =  true,
-              CategoryId = 1,
-              Category = new Category
-              {
-                  Id = 1,
-                  Name = "Fruits"
-              }
-            }
-        };
+        // this basically assigns a varialble to type IConfiguration
+        private readonly IConfiguration _config;
 
-        return Ok(products);
+    // this gives the settings in .json (where the database is)
+    public ProductController(IConfiguration config) 
+    {
+        _config = config;
     }
+
+    [HttpGet]
+    //gets a http response where in this case, getting the database connection called "DefaultConnection"
+    public async Task<IActionResult> Get()
+    {
+        var connStr = _config.GetConnectionString("DefaultConnection");
+        // to make sure that we know theres a problem if we're unable to fetch it
+        if (string.IsNullOrWhiteSpace(connStr))
+            return Problem("Missing connection string: DefaultConnection");
+
+        // executes these set of statements to retrieve the database.
+        const string sql = @"
+        SELECT p.Id, p.Name, p.Price, p.Quantity, p.InStock, p.CategoryId, p.CreatedDate,
+        c.Id AS Category_Id, c.Name AS Category_Name
+        FROM Products p
+        JOIN Categories c ON p.CategoryId = c.Id 
+        ORDER BY p.Id;"; 
+    }
+}
 }
