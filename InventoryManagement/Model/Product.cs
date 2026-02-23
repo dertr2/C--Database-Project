53c7103be1feb8bda6d11090064c2fe404a8518c
Michael Yan <myan2006@students.cs.ubc.ca>
2026-02-23 10:55:27 -0800

    Added getters and setters, added string.Empty to prevent null values appearing from reference types. Also changed category property to a seperate "table" where it can have its own id and "join" it with the products.

--- InventoryManagement/Model/Product.cs
+++ InventoryManagement/Model/Product.cs
@@ -2,11 +2,12 @@ namespace InvencotryManagement.Model;
 
 public class Product
 {
-    public int Id;
-    public string Name;
-    public decimal Price;
-    public int Quantity;
-    public bool InStock;
-    public string Catagory;
-    public DateTime CreatedDate;
+    public int Id { get; set; } 
+    public string Name {get; set;} = string.Empty;
+    public decimal Price {get; set;}
+    public int Quantity {get; set;}
+    public bool InStock {get; set;}
+    public int CategoryId { get; set; }
+    public Category? Category { get; set; }
+    public DateTime CreatedDate {get; set;}
 }
